d84e26b33c3cb3a5a6c25973b64a74224a27acf1
sedifexbiz <sedifexbiz@gmail.com>
2025-09-23 17:41:49 +0000

    Update main.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
+import Products from './pages/Products'
 
 const router = createBrowserRouter([
   { path: '/', element: <App /> },
+  { path: '/products', element: <Products /> },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
