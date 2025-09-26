3f61692efa1a1b8cf9f6579b9b1a2e884a935bc4
sedifexbiz <sedifexbiz@gmail.com>
2025-09-26 15:32:42 +0000

    Delete web directory

--- web/src/hooks/useAuthUser.ts
+++
@@ -1,8 +0,0 @@
-import { createContext, useContext } from 'react'
-import type { User } from 'firebase/auth'
-
-export const AuthUserContext = createContext<User | null>(null)
-
-export function useAuthUser() {
-  return useContext(AuthUserContext)
-}
